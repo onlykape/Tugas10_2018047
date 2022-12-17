24d1df3df52766f2a093be11ae98f8022eb27fd6
onlykape <kpranata141@gmail.com>
2022-12-17 17:43:15 +0700

    last prak :)

---
+++ app/src/main/java/com/example/mobileprak_tugas5/MainActivity5.java
@@ -0,0 +1,52 @@
+package com.example.mobileprak_tugas5;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import com.example.mobileprak_tugas5.databinding.ActivityMain5Binding;
+import android.widget.Toast;
+
+public class MainActivity5 extends AppCompatActivity implements View.OnClickListener {
+    MyDbHelper myDbHelper;
+    private ActivityMain5Binding binding;
+    private SessionManager session;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setup view binding
+        binding = ActivityMain5Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        session = new SessionManager(getApplicationContext());
+        if (session.isLoggedIn()) {
+            Intent intent = new Intent(MainActivity5.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        session = new SessionManager(getApplicationContext());
+        myDbHelper = new MyDbHelper(this);
+        SQLiteDatabase sqLiteDatabase = myDbHelper.getWritableDatabase();
+        binding.Signinbuttonid.setOnClickListener(this);
+        binding.SignUpHerebuttonid.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        String username = binding.signinusernameEditText.getText().toString();
+        String password = binding.signinpasswordEditText.getText().toString();
+        if (v.getId() == R.id.Signinbuttonid) {
+            Boolean result = myDbHelper.findPassword(username, password);
+            if (result == true) {
+                Intent intent = new Intent(MainActivity5.this, MainActivity.class);
+                startActivity(intent);
+                session.setLogin(true);
+                finish();
+            } else {
+                Toast.makeText(this, "username and password didn`t match", Toast.LENGTH_SHORT).show();
+            }
+        } else if (v.getId() == R.id.SignUpHerebuttonid) {
+            Intent intent = new Intent(MainActivity5.this, RegisterActivity.class);
+            startActivity(intent);
+        }
+    }
+}
